7e5ef76663f1b932ecaa07638dcda7fea47a0f9b
Martin <cheyuan0621@icloud.com>
2023-05-15 11:17:21 +0800

    modified:   login.php

--- login.php
+++ login.php
@@ -9,14 +9,11 @@ $sql="select count(*) from `members` where `acc`='$acc' && `password`='$pw'";
 echo $sql;
 
 $chk=$pdo->query($sql)->fetchColumn();
-// echo "<pre>";
-// print_r($user);
-// echo "</pre>"; 
-// echo $chk;
+
 if($chk){
     echo "登入成功";
 }else{
     echo "登入失敗，帳號或密碼錯誤";
 }
 
-// header("location:index.html");
+header("location:index.html");
